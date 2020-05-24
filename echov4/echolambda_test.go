583abf9a4c5da53e36692073d326ef3d1085ccb5
Kurt McAlpine <kurt@linux.com>
2020-05-24 15:54:11 +1200

    Update test

--- echov4/echolambda_test.go
+++ echov4/echolambda_test.go
@@ -34,23 +34,27 @@ var _ = Describe("EchoLambda tests", func() {
 			Expect(resp.StatusCode).To(Equal(200))
 		})
 	})
-	Context("Simple ping request", func() {
+	Context("Simple ping request v2", func() {
 		It("Proxies the event correctly", func() {
 			log.Println("Starting test")
 			e := echo.New()
-			e.GET("/ping", func(c echo.Context) error {
+			e.GET("/v1/ping", func(c echo.Context) error {
 				log.Println("Handler!!")
 				return c.String(200, "pong")
 			})
 
 			adapter := echov4adapter.New(e)
 
-			req := events.APIGatewayProxyRequest{
-				Path:       "/ping",
-				HTTPMethod: "GET",
+			req := events.APIGatewayV2HTTPRequest{
+				RequestContext: events.APIGatewayV2HTTPRequestContext{
+					HTTP: events.APIGatewayV2HTTPRequestContextHTTPDescription{
+						Path:   "/v1/ping",
+						Method: "GET",
+					},
+				},
 			}
 
-			resp, err := adapter.Proxy(req)
+			resp, err := adapter.ProxyV2(req)
 
 			Expect(err).To(BeNil())
 			Expect(resp.StatusCode).To(Equal(200))
